28bacea8213f176b5d4a1986ad12269ccc9850d3
abdulhadi8641 <Abdul.hadi@dexterztechnologies.com>
2026-07-21 21:13:21 +0500

    feat: update color scheme to purple to match iStream logo brand identity

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  trailingSlash: true,
   images: {
+    unoptimized: true,
     qualities: [75, 85, 95, 100],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
